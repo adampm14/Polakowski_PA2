dcc6ec4f97ad62997cc117ead606b8f8bcfab8e7
adamp <adamp@66.158.237.35>
2020-02-04 20:40:17 -0500

    second question

---
+++ Polakowski_p2/src/BMICalculator.java
@@ -0,0 +1,98 @@
+import java.util.Scanner;
+public class BMICalculator {
+
+	
+	
+
+public void readUserData() {
+	System.out.println("choose metric or imperial");
+	
+
+	
+	
+}
+
+private void readUnitType() {
+		
+		
+	
+	
+}
+	
+private void readMeasurementData(){
+		
+		
+}
+
+
+private void readMetricData() {
+
+
+
+}
+
+private void readImperialData() {
+
+
+}
+
+public void calculateBmi() {
+
+
+
+	
+}
+
+
+private void calculateBmiCategory() {
+
+
+
+}
+
+
+public void displayBmiCategory() {
+
+
+	
+}
+
+public void getWeight() {
+
+
+
+}
+
+private void setWeight(){
+	
+	
+}
+
+public void getHeight() {
+	
+	
+}
+
+private void setHeight() {
+	
+	
+	
+}
+
+public void getBmi() {
+
+
+}
+public void getBmiCategory() {
+	
+	
+	
+	
+}
+}
+//Pseodcode:
+//read user input to decide if the system is metric or imperial
+//if imperial use imperial measurements if not use metric
+//prompt user to enter their weight and height
+//enter weight and height integers into BMI equation
+//output BMI process the category of BMI
